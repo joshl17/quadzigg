9a2050f22ce1ba934d0277a2f80b0963fb1c1fd8
Ubuntu <ubuntu@ip-13-0-1-250.eu-west-2.compute.internal>
2024-03-26 11:34:03 +0000

    Added bypass for web certificate and disabled legacy connection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
   skip_before_action :redirect_to_signin
   def signin
-    redirect_to cognito_signin_url
+    redirect_to cognito_signin_url, allow_other_host: true
   end
 
   def signout
-    redirect_to cognito_signout_url
+    redirect_to cognito_signout_url, allow_other_host: true
   end
 
   def signup
-    redirect_to cognito_signup_url
+    redirect_to cognito_signup_url, allow_other_host: true
   end
 
 private
